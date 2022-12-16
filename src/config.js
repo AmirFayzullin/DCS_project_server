74f97bee9252fb709c708efa6a8f2810e73257a0
Amir <amir.fayzullin@gmail.com>
2022-12-16 15:35:53 +0500

    jwt

--- src/config.js
+++ src/config.js
@@ -8,5 +8,6 @@ module.exports = {
     WEB_SOCKET_SERVER_PORT: 5001,
     INPUTS_FOLDER: path.join(__dirname, "..", "files", "inputs"),
     OUTPUTS_FOLDER: path.join(__dirname, "..", "files", "outputs"),
-    DB_URL: path.join(__dirname, "..", "db", "db")
+    DB_URL: path.join(__dirname, "..", "db", "db"),
+    JWT_KEY: 'a62b-8a5b-f6ab-3c4d'
 };
--- src/expressServer/expressServer.js
+++ src/expressServer/expressServer.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const rimraf = require("rimraf");
+const jwt = require('jsonwebtoken');
 const app = express();
 const {run: runWSServer} = require("../webSocketServer/webSocketServer");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 const formidable = require('formidable');
-const {HTTP_SERVER_PORT, INPUTS_FOLDER, OUTPUTS_FOLDER, DB_URL} = require("../config");
+const {HTTP_SERVER_PORT, INPUTS_FOLDER, OUTPUTS_FOLDER, DB_URL, JWT_KEY} = require("../config");
 const ImagesOptimizer = require("../components/ImagesOptimizer/ImagesOptimizer");
 const JimpOptimizerStrategy = require("../components/ImagesOptimizer/JimpOptimizerStrategy");
 const FilesCollection = require("../components/FilesCollection/FilesCollection");
@@ -46,12 +47,65 @@ const createPDF = ({outputFilepath, inputFolder, processingParams}) => {
     });
 };
 
+app.useP = function(...args) {
+    function wrap(fn) {
+        return async function(req, res, next) {
+            // catch both synchronous exceptions and asynchronous rejections
+            try {
+                await fn(req, res, next);
+            } catch(e) {
+                next(e);
+            }
+        }
+    }
+
+    // reconstruct arguments with wrapped functions
+    let newArgs = args.map(arg => {
+        if (typeof arg === "function") {
+            return wrap(arg);
+        } else {
+            return arg;
+        }
+    });
+    // register actual middleware with wrapped functions
+    app.use(...newArgs);
+};
+
+const findUser = (req) => {
+    return new Promise((res, rej) => {
+        if (req.headers.authorization) {
+            jwt.verify(
+                req.headers.authorization.split(' ')[1],
+                JWT_KEY,
+                async (err, payload) => {
+                    if (err) res();
+                    else if (payload) {
+                        try {
+                            const user = await dbapi.user.getById({id: payload.id});
+                            res(user);
+                        } catch(err) {
+                            console.log(err);
+                            res();
+                        }
+                    }
+                }
+            )
+        } else {
+            res();
+        }
+    });
+};
+
+app.useP(async (req, res, next) => {
+    req.user = await findUser(req);
+    next();
+});
+
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 app.use(cors());
-
 
 app.post("/uploadImages", (req, res) => {
     const form = new formidable.IncomingForm();
@@ -88,7 +142,7 @@ app.post("/uploadImages", (req, res) => {
         }
     });
 
-    res.send({status: "ok"});
+    res.json({status: 0});
 });
 
 app.post("/register", async (req, res) => {
@@ -99,14 +153,14 @@ app.post("/register", async (req, res) => {
         const existingUser = await dbapi.user.get({email});
 
         if (existingUser) {
-            response.status(200).send(JSON.stringify({status: 1, messages: ["User exists"]}));
+            response.status(200).json({status: 1, messages: ["User exists"]});
             return;
         }
 
         await dbapi.user.add({email, password});
-        response.status(201).send(JSON.stringify({status: 0, messages: ["Success"]}));
+        response.status(201).json({status: 0, messages: ["Success"]});
     } catch(err) {
-        response.status(500).send(JSON.stringify({status: 1, messages: [err.message]}));
+        response.status(500).json({status: 1, messages: [err.message]});
     }
 });
 
@@ -118,19 +172,37 @@ app.post("/login", async (req, res) => {
         const existingUser = await dbapi.user.get({email});
 
         if (!existingUser || existingUser.password !== password) {
-            response.status(200).send(JSON.stringify({status: 1, messages: ["Invalid email or password"]}));
-            return;
+            return response.status(200).json({status: 1, messages: ["Invalid email or password"]});
         }
 
-        response.status(200).send(JSON.stringify({status: 0, messages: ["Success"]}));
+        response.status(200).send(JSON.stringify({
+            status: 0,
+            messages: ["Success"],
+            token: jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+                id: existingUser.id
+            }, JWT_KEY)
+        }));
     } catch(err) {
-        response.status(500).send(JSON.stringify({status: 1, messages: [err.message]}));
+        response.status(500).json({status: 1, messages: [err.message]});
     }
 });
 
 app.get('/download', function(req, res){
-    const file = `${OUTPUTS_FOLDER}/output.pdf`;
+    const file = `${OUTPUTS_FOLDER}/output.pdf`
     res.download(file, 'output.pdf');
+});
+
+app.get('/me', async (req, res) => {
+    const response = res.setHeader("Content-Type", "application/json");
+
+    if (!req.user) return response.json({status: 1, messages: ["Unauthorized"]});
+
+    response.send(JSON.stringify({
+        status: 0,
+        messages: ["Authorized"],
+        email: req.user.email
+    }));
 });
 
 const run = () => {
